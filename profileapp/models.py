cea362b66f3c99da1e0ce16e3e8277e7a96ba96a
kss29 <kss293027@gmail.com>
2021-07-26 10:08:52 +0900

    7/26 5th class commit

--- profileapp/models.py
+++ profileapp/models.py
@@ -1,3 +1,12 @@
+from django.contrib.auth.models import User
 from django.db import models
 
 # Create your models here.
+
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE, related_name='profile')
+    # user 와 1대1로 연결 / on_del : 이게 지워질경우 어떻게 할것인가, 삭제 정책 담당 (CASCADE = 종속)
+
+    image = models.ImageField(upload_to='profile/', null=True)
+    nickname = models.CharField(max_length=30, unique=True, null=True) #Charfiled 의 필수값 : max_length
+    message = models.CharField(max_length=200, null=True)
