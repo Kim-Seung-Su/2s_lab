5421b57bdb5ea6a56c620e47985e69eefce6e8b5
Kim Seung Soo <kss293027@gmail.com>
2021-08-05 10:48:57 +0900

    8/ 5th my image get in list / hide update button / create article button

--- articleapp/views.py
+++ articleapp/views.py
@@ -1,14 +1,13 @@
 from django.contrib.auth.decorators import login_required
-from django.shortcuts import render
 
 # Create your views here.
 from django.urls import reverse_lazy, reverse
 from django.utils.decorators import method_decorator
-from django.views.generic import CreateView, DetailView, UpdateView, DeleteView
+from django.views.generic import CreateView, DetailView, UpdateView, DeleteView, ListView
 
 from articleapp.decorators import article_ownership_required
 from articleapp.models import Article
-from articleapp.templates.foms import ArticleCreationForm
+from articleapp.foms import ArticleCreationForm
 
 
 @method_decorator(login_required,'get')
@@ -49,3 +48,10 @@ class ArticleDeleteView(DeleteView):
     context_object_name = 'target_article'
     success_url = reverse_lazy('articleapp:list')
     template_name = 'articleapp/delete.html'
+
+
+class ArticleListView(ListView):
+    model = Article
+    context_object_name = 'article_list'
+    template_name = 'articleapp/list.html'
+    paginate_by = 20
